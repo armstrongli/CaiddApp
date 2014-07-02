08f3e6c60688959e938234a750323e5054a8a8f0
armstrongli <ali2@vmware.com>
2014-07-02 17:12:35 +0800

    modify the errors

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
-public static void main(String[] args){
-	System.out.println("this is a test file. just test for git branch and commit");
+public class Test {
+	public static void main(String[] args) {
+		System.out
+				.println("this is a test file. just test for git branch and commit");
 
-    System.out.println("which still chontais errors, but i'll test more file conflicts.");
+		System.out
+				.println("which still chontais errors, but i'll test more file conflicts.");
+	}
 }
