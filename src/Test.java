101e6aae0ffc7a274b4fd624b72bb288e5eabed5
armstrongli <ali@L-ALI-MAC.local>
2014-07-02 14:03:40 +0800

    add more file message

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
 public static void main(String[] args){
 	System.out.println("this is a test file. just test for git branch and commit");
+
+    System.out.println("which still chontais errors, but i'll test more file conflicts.");
 }
